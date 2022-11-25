895b9ba923bb1616646ed06f6c029c7d87b9cd93
Darius Vasionis <darius@vasionis.eu>
2022-11-25 15:23:52 +0200

    Pabaigta

--- ptu5_postit/postit_api/serializers.py
+++ ptu5_postit/postit_api/serializers.py
@@ -14,7 +14,7 @@ class CommentSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source="user.username")
     user_id = serializers.ReadOnlyField(source='user.id')
-    comments = CommentSerializer(many=True)
+    comments = CommentSerializer(many=True, read_only=True)
     comments_count = serializers.SerializerMethodField()
     # comments = serializers.StringRelatedField(many=True)
 
